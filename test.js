4370217dea593540b11b02f6f01e609173a756d6
KangggX <s10205347@connect.np.edu.sg>
2021-02-15 18:47:58 +0800

    working on alternative quiz script

---
+++ test.js
@@ -0,0 +1,49 @@
+var quiz1 = [
+	{
+		question: "How does COVID-19 spread among us?",
+		answers: {
+			a: "Through droplets that come from our body such as cough droplets and breath",
+			b: "Sexual Fluids",
+			c: "Water that is not clean",
+            d: "All of the above"
+		},
+		correctAnswer: "a"
+	},
+	{
+		question: "What are some of the common symptoms of COVID-19?",
+		answers: {
+			a: "Coughing",
+			b: "Fever",
+			c: " Feeling Tired",
+            d: "All of the above "
+		},
+		correctAnswer: "d"
+	},
+    {
+		question: "Can you tell when someone has COVID-19 without actual testing?",
+		answers: {
+			a: "No, not everyone will have symptoms when they contract Covid-19.",
+			b: "Yes, it is easy to tell just by looking at how much the person coughs."
+		},
+		correctAnswer: "a"
+	},
+    {
+		question: "Can washing your hands protect you from contracting COVID-19?",
+		answers: {
+			a: "Yes, only if you use soap or hand sanitizer.",
+			b: "No, Washing your hands does not kill Covid-19."
+		},
+		correctAnswer: "a"
+	},
+    {
+		question: "Which age group is more vulnerable to COVID-19?",
+		answers: {
+			a: "Older people and people with underlying health conditions",
+			b: "Childrens",
+			c: "Teenagers",
+            d: "All of the above"
+		},
+		correctAnswer: "a"
+	}
+];
+
